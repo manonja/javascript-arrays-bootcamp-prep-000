f4cfda9c3cf1666f5e2766ac34206836024d96d0
manon jacquin <manon.jacquin@gmail.com>
2018-08-10 23:53:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -68,3 +68,8 @@ function destructivelyRemoveElementFromEndOfArray(array){
   array.pop();
   return array;
 }
+
+function removeElementFromEndOfArray(array){
+  array.slice(0, array.length - 1)
+}
+
